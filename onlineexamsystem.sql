676327d4e04ca8c3be7c1e75cba53c93619ed175
Sarfraz Rasheed <56587381+Sarfraz123-wq@users.noreply.github.com>
2022-08-14 23:59:20 +0500

    Database updated

--- onlineexamsystem.sql
+++ onlineexamsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 12, 2022 at 05:11 PM
+-- Generation Time: Aug 14, 2022 at 08:58 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `onlineexamsystem`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exams`
+--
+
+CREATE TABLE `exams` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `exam_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `subject_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `time` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `exams`
+--
+
+INSERT INTO `exams` (`id`, `exam_name`, `subject_id`, `date`, `time`, `created_at`, `updated_at`) VALUES
+(12, 'C Test', '1', '2022-08-22', '14:00', '2022-08-14 13:57:34', '2022-08-14 13:57:34'),
+(13, 'Quiz', '2', '2022-08-22', '14:00', '2022-08-14 13:57:47', '2022-08-14 13:57:47');
 
 -- --------------------------------------------------------
 
@@ -57,7 +81,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(8, '2022_08_12_152137_create_subjects_table', 2),
+(9, '2022_08_14_064024_create_exams_table', 2);
 
 -- --------------------------------------------------------
 
@@ -93,6 +119,29 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `subject_id` bigint(20) UNSIGNED NOT NULL,
+  `subject_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`subject_id`, `subject_name`, `created_at`, `updated_at`) VALUES
+(1, 'Computer', '2022-08-14 04:48:46', '2022-08-14 04:48:46'),
+(2, 'Mechanics', '2022-08-14 04:48:53', '2022-08-14 05:06:38'),
+(3, 'Maths', '2022-08-14 05:06:17', '2022-08-14 05:07:26'),
+(4, 'Data', '2022-08-14 05:59:51', '2022-08-14 05:59:51');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -114,11 +163,18 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `is_admin`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Admin', 'admin@admin.com', '1', NULL, '$2y$10$1JeWO.W.sITCRSw0njQ4ae2/XnliVYYlcOnp1mh/73yl9eAeu9AKK', NULL, '2022-08-12 08:47:04', '2022-08-12 08:47:04'),
-(2, 'Sarfraz Rasheed', 'keharsarfraz@gmail.com', '0', NULL, '$2y$10$EucUBVeMeaaFDS0M7SlhVuDDyR91tYzsaWo4wsDI29vBXcgocUN9a', NULL, '2022-08-12 08:47:36', '2022-08-12 08:47:36');
+(2, 'Sarfraz Rasheed', 'keharsarfraz@gmail.com', '0', NULL, '$2y$10$EucUBVeMeaaFDS0M7SlhVuDDyR91tYzsaWo4wsDI29vBXcgocUN9a', NULL, '2022-08-12 08:47:36', '2022-08-12 08:47:36'),
+(3, 'Tahir Rasheed', 'kehartahir@gmail.com', '0', NULL, '$2y$10$FPuAVwgcCEwIu0NLeOb7EOEUUnvUVxbx7xGY8iAfakml8bbiXy54W', NULL, '2022-08-12 10:17:56', '2022-08-12 10:17:56');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `exams`
+--
+ALTER TABLE `exams`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `failed_jobs`
@@ -148,6 +204,12 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`subject_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -159,6 +221,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `exams`
+--
+ALTER TABLE `exams`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
 -- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
@@ -168,7 +236,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -177,10 +245,16 @@ ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `subject_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
